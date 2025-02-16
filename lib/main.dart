99c2713ab2ac5a4b63b16e87b2ae03f52cd54b3f
Sumiara5365 <rafiq15-5365@diu.edu.bd>
2025-02-16 12:08:21 +0600

    task 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const AppHome());
 }
 
-class MyApp extends StatelessWidget {
+class AppHome extends StatelessWidget {
+  const AppHome({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Star Rating',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lab 05 - Flutter"),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              size: 40,
+              Iconsax.activity4,
+              color: Colors.blue,
+            ),
+            Icon(
+              size: 40,
+              Iconsax.activity4,
+              color: Colors.blue,
+            ),
+            Text(
+              "This is a ",
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 30,
+              ),
+            ),
+            Icon(
+              size: 30,
+              Iconsax.add_circle,
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
-      home: StarRatingPage(),
     );
   }
 }
